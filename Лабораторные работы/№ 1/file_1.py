900715473b35c11090b0f097cf62d60c80adf5b2
Denis <denisbuhanov4@gmail.com>
2023-09-10 15:58:23 +0900

    Реализация 1 задания + образец

---
+++ Лабораторные работы/№ 1/file_1.py
@@ -0,0 +1,12 @@
+
+
+
+
+x = int(input("Введите значение x: "))
+y = int(input("Введите значение y: "))
+
+print("{0}+{1}=?".format(y, x))
+print("{0} | {1} | {2}".format(y, x, x + y))
+print("Z({0})=F({1})".format(x, y))
+print("x={0}; y={1};".format(x, y))
+print("Ответ: ({0};{1})".format(x, y))
