9ae564a9a2cec7a5485a3fb00337b9d5fc4ef53e
Denis <denisbuhanov4@gmail.com>
2023-09-17 14:42:47 +0300

    Выполнение дз
    Лабораторная работа № 2

--- Лабораторные работы/№ 2/file_4.py
+++ Лабораторные работы/№ 2/file_4.py
@@ -4,8 +4,8 @@
 
 
 N = 10
-lst_sum = [] # Список для хранения будующих данных
+lst_sum = [] # Список для хранения будущих данных
 for i in range(N):
-    lst_sum.append(int(input("Введите количество случайных цифр: ")))
+    lst_sum.append(int(input("Введите значение цифры: ")))
     
 print(f"Среднее арифметическое: {sum(lst_sum) / len(lst_sum)}")
--- Лабораторные работы/№ 2/file_5.py
+++ Лабораторные работы/№ 2/file_5.py
@@ -5,9 +5,9 @@
 import random
 
 
-N = 10
-lst_sum = [] # Список для хранения будующих данных
+N = 10 # количество итераций цикла
+lst_sum = [] # Список для хранения будущих данных
 for i in range(N):
-    lst_sum.append(random.randint(-1_000, 1_000))
+    lst_sum.append(random.randint(0, 1_000))
     
-print(f"Среднее арифметическое: {sum(lst_sum) / len(lst_sum)}")
+print(f"Среднее арифметическое случайных чисел: {sum(lst_sum) / len(lst_sum)}")
