ac55e5bf96d431fe91ca4bc2a24d0d3c2cec8268
Denis <denisbuhanov4@gmail.com>
2023-10-03 09:28:27 +0900

    Исправление недочетов

--- Лабораторные работы/№ 3/file2.py
+++ Лабораторные работы/№ 3/file2.py
@@ -2,14 +2,12 @@
 
 
 
-def divider(number:int) -> str:
+def divider(number:int) -> None:
     """Процедуру, которая определяет все делители передаваемого ей числа (в одну строчку)
 
     Args:
         number (int): передаваемое число
 
-    Returns:
-        str: строка с делителями
     """
     res = ""
     for i in range(1, abs(number)+1):
@@ -17,22 +15,9 @@ def divider(number:int) -> str:
             res += f"{i} "
             
     if len(res) == 0:
-        return "Таких чисел нет"
-    return res
+        print("Таких чисел нет")
+    print("Делители:", res)
 
 
 if __name__ == "__main__": # Если скрипт запущен как основной
-    print("Делители:", divider(int(input("Введите число: "))))
-    
-    
-    def Test():
-        tests = [(7, ), (-5, ), (0, )]
-        result = ["1 7 ", "1 5 ", "Таких чисел нет"]
-        k = 0
-        for i in tests:
-            res = divider(*i)
-            assert  res == result[k], f"Test {k}"
-            k += 1
-    
-    
-    # Test()
+    divider(int(input("Введите число: ")))
--- Лабораторные работы/№ 3/file3.py
+++ Лабораторные работы/№ 3/file3.py
@@ -2,37 +2,21 @@
 
 
 
-def in_degree(number:int, degree:float) -> int:
+def in_degree(number:int, degree:float) -> None:
     """Процедура, которая вычисляет степень числа 
 
     Args:
         number (int): передаваемое число
         degree (float): передаваемая степень
 
-    Returns:
-        int: число в степени
     """
     if number < 0 and (degree ** (-1)) % 2 == 0:
-        return "Невозможно из отрицательного числа вычислить четный корень"
+        print("Невозможно из отрицательного числа вычислить четный корень")
     
-    return number ** degree
+    print("Число в степени", number ** degree)
 
 
 if __name__ == "__main__": # Если скрипт запущен как основной
-    print(in_degree(int(input("Введите число: ")), 
-                    float(input("Введите степень числа: "))
-                    )
-          )
-    
-    
-    def Test():
-        tests = [(7, 2), (-25, 0.5), (205, 0.5)]
-        result = [49, "Невозможно из отрицательного числа вычислить четный корень", 5]
-        k = 0
-        print("Tests")
-        for i in tests:
-            res = in_degree(*i)
-            assert  res == result[k], f"Test {k}"
-            k += 1
-        
-    # Test()
+    in_degree(int(input("Введите число: ")), 
+              float(input("Введите степень числа: ")))
+
