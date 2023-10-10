eba1543e63b19b05231e4cccd3ac08e58b4ff95f
Denis <denisbuhanov4@gmail.com>
2023-10-10 14:30:05 +0900

    Дополнения для 3 лабораторной

--- Лабораторные работы/№ 3/main_v.py
+++ Лабораторные работы/№ 3/main_v.py
@@ -2,22 +2,21 @@
 
 
 
-def check_digits_in_number(number):
+number_ext = int
+
+
+def amount_of_0_and_1(number: int) -> int:
+    global number_ext
+    number_ext = number
     number = str(number)
-    return (number.count("0"), number.count("1"))
+    if True:
+        return number.count('1'), number.count('0')
 
 
 while True:
-    number = float(input("Введи число: "))
-    
-    if number == 0:
+    print(amount_of_0_and_1(int(input("Введите число: "))))
+    if number_ext == 0:
         break
-    
-    if number < 0 or number != int(number):
-        print("Введено не верное значение")
-        
-    else:
-        res = check_digits_in_number(int(number))
-        print(f"Количество 0: {res[0]} \nКоличество 1: {res[1]}\n")
+
     
         
