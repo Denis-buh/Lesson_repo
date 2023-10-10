0fc58f0d95aeae6c329d1f4fcd201ff786a4e047
Denis <denisbuhanov4@gmail.com>
2023-10-10 15:20:49 +0900

    Create file1.py

---
+++ Лабораторные работы/№ 4/file1.py
@@ -0,0 +1,12 @@
+
+
+
+
+def main():
+    # Генерируем список
+    lst = [i for i in range(-20, 31)]
+    print(f"Сгенерированный список: \n{lst}")
+    
+    
+if __name__ == "__main__":
+    main()
