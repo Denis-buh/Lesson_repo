84ecee5873c76bce09036ab49a0a4356617c9e4d
Denis <denisbuhanov4@gmail.com>
2023-10-18 00:49:54 +0900

    Дополненая дз для 2

--- Лабораторные работы/№ 2/main_2.py
+++ Лабораторные работы/№ 2/main_2.py
@@ -10,4 +10,4 @@ for i in number:
     if int(i) % 4 == 0:
         flag += 1
         
-print(f"Колличество цифр кратных 4: {flag}")
+print(f"Количество цифр кратных 4: {flag}")
--- Лабораторные работы/№ 2/main_3.py
+++ Лабораторные работы/№ 2/main_3.py
@@ -12,16 +12,24 @@ print("-" * 60)
 
 for i in range(-500, 1000, 55):
     i /= 100
-    # print(i)
+
     if i < -1:
-        print("!{: ^28}!".format(i), "{: ^29}!".format(i ** 2))
+        print(
+            "!{: ^28}!".format(i), 
+            "{: ^29}!".format(
+                "{: .3f}".format(i ** 2)
+            )
+        )
         
     if 0 <= i <= 12.5:
-        print("!{: ^28}!".format(i), 
-              "{: ^29}!".format((math.e ** i) + 5 + math.cos(0.001 * i)))
+        print(
+            "!{: ^28}!".format(i), 
+            "{: ^29}!".format(
+                "{: .3f}".format((math.e ** i) + 5 + math.cos(0.001 * i))
+            )     
+        )
         
     '''if i > 12.5:
         print("{: ^30}".format(i), 
               "{: ^30}".format((i ** 2) - 3 + (2.5 * (i ** 2))))'''
         
-    
