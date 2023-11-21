82d3bdca7bf87fa1358abcef993aa0d7722e3ba9
Denis <denisbuhanov4@gmail.com>
2023-11-21 14:43:19 +0900

    оформлена 6

--- Лабораторные работы/№ 7/file3.py
+++ Лабораторные работы/№ 7/file3.py
@@ -10,7 +10,11 @@ def main():
     ls = []
     LEN = 20
     for i in range(LEN):
-        ls.append(random.random())
+        ls.append(random.randint(0, 4))
+    min_item_index = ls.index(min(ls))
+    max_item_index = ls.index(max(ls))
+    print(f"Номер первого минимального элемента массива {min_item_index + 1}")
+    print(f"Номер последнего максимального элемента массива {max_item_index + 1}")
     
 
 
